d812fc242968acbff53517094b312f7aeeb634a1
Justin Barnett <justin.barnett@268generation.com>
2026-05-20 06:47:25 -0400

    feat(nvim): add window split, resize, and close keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,6 +11,23 @@ map("n", "<C-j>", "<C-w>j", { desc = "Window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "Window up" })
 map("n", "<C-l>", "<C-w>l", { desc = "Window right" })
 
+map("n", "<leader>-", "<cmd>split<cr>", { desc = "Split horizontal" })
+map("n", "<leader>|", "<cmd>vsplit<cr>", { desc = "Split vertical" })
+
+local function push_divider(axis, sign)
+  local probe = axis == "h" and "l" or "j"
+  local has_next = vim.fn.winnr() ~= vim.fn.winnr(probe)
+  local delta = has_next and sign or -sign
+  local cmd = axis == "h" and "vertical resize " or "resize "
+  vim.cmd(cmd .. (delta > 0 and "+" or "") .. delta * 2)
+end
+
+map("n", "<C-Right>", function() push_divider("h", 1) end, { desc = "Push split divider right" })
+map("n", "<C-Left>", function() push_divider("h", -1) end, { desc = "Push split divider left" })
+map("n", "<C-Down>", function() push_divider("v", 1) end, { desc = "Push split divider down" })
+map("n", "<C-Up>", function() push_divider("v", -1) end, { desc = "Push split divider up" })
+map("n", "<C-x>", "<cmd>close<cr>", { desc = "Close split" })
+
 map("n", "[b", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
 
